a1558ee3ab11ebb48ecbac5335141c378e8e7ff5
Jonas Hammerschmidt <jonas.hammerschmidt@outlook.de>
2021-03-27 10:43:57 +0100

    Updated test script

--- test.ps1
+++ test.ps1
@@ -3,13 +3,19 @@ Set-Location Contract.Architecture\Contract.Architecture.Backends\Contract.Archi
 contractor add domain Bankwesen
 contractor add entity Bankwesen.Bank:Banken
 contractor add property string:256 Name -e Bankwesen.Bank:Banken
-contractor add property bool IsLiquide -e Bankwesen.Bank:Banken
+contractor add property DateTime EroeffnetAm -e Bankwesen.Bank:Banken
+contractor add property bool IsPleite -e Bankwesen.Bank:Banken
+
+contractor add entity Bankwesen.Konto:Konten
+contractor add property string:256 Name -e Bankwesen.Konto:Konten
+contractor add property DateTime EroeffnetAm -e Bankwesen.Konto:Konten
 
 contractor add domain Kundenstamm
 contractor add entity Kundenstamm.Kunde:Kunden
 contractor add property string:256 Name -e Kundenstamm.Kunde:Kunden
-contractor add property DateTime Geburtstag -e Kundenstamm.Kunde:Kunden
+contractor add property int Balance -e Kundenstamm.Kunde:Kunden
 
 contractor add relation 1:n Bankwesen.Bank:Banken Kundenstamm.Kunde:Kunden
+contractor add relation 1:n Bankwesen.Konto:Konten Kundenstamm.Kunde:Kunden
 
 Set-Location ..\..\..
