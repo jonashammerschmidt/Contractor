0a2e28c61dccd6d5e6914d3fe9433c640651eb0c
Jonas Hammerschmidt <jonas.hammerschmidt@outlook.de>
2021-04-06 09:44:56 +0200

    Improved default generation

--- Contractor.Core/Tools/PropertyLines/FrontendDtoDefaultPropertyLine.cs
+++ Contractor.Core/Tools/PropertyLines/FrontendDtoDefaultPropertyLine.cs
@@ -1,6 +1,5 @@
 ﻿using Contractor.Core.Helpers;
 using Contractor.Core.Options;
-using System;
 
 namespace Contractor.Core.Tools
 {
@@ -8,32 +7,12 @@ namespace Contractor.Core.Tools
     {
         public static string GetPropertyLine(IPropertyAdditionOptions options)
         {
-            if (options.IsOptional && options.PropertyType != PropertyTypes.String)
+            if (options.PropertyType == PropertyTypes.String)
             {
                 return $"    {options.PropertyName.LowerFirstChar()}: '',";
             }
 
-            switch (options.PropertyType)
-            {
-                case PropertyTypes.String:
-                    return $"    {options.PropertyName.LowerFirstChar()}: '',";
-
-                case PropertyTypes.Integer:
-                    return $"    {options.PropertyName.LowerFirstChar()}: 0,";
-
-                case PropertyTypes.Double:
-                    return $"    {options.PropertyName.LowerFirstChar()}: 0.0,";
-
-                case PropertyTypes.DateTime when !options.IsOptional:
-                    return $"    {options.PropertyName.LowerFirstChar()}: new Date(),";
-
-                case PropertyTypes.Boolean:
-                case PropertyTypes.Guid:
-                    return $"    {options.PropertyName.LowerFirstChar()}: null,";
-
-                default:
-                    throw new NotImplementedException();
-            }
+            return $"    {options.PropertyName.LowerFirstChar()}: null,";
         }
     }
 }
--- Contractor.Docs/TODOs.md
+++ Contractor.Docs/TODOs.md
@@ -4,6 +4,9 @@ Main Quests:
 - contractor add relation 1:n 
   - [-n Vertragsbank:Vertragskunden] Alternative Property Names
 
+Try:
+  - Frontend-DTOs as Classes
+
 Ideas: 
 - contractor add relation n:m UserManagement.User:Users UserManagement.Group:Groups UserGroupMembership
 - contractor add relation 1:1 (In Logic/API sinnvoll, in DB ist es trotzdem nur eine Tabelle. Da es sehr generisch ist und viel Aufwand bedeutet, kommt das später.
