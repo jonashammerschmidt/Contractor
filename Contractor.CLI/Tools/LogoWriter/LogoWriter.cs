569ab5bab6879382f84549c87d297fac32693245
Jonas Hammerschmidt <j.hammerschmidt@krz.de>
2022-04-18 09:15:58 +0200

    Simplified LogoWriter

--- Contractor.CLI/Tools/LogoWriter/LogoWriter.cs
+++ Contractor.CLI/Tools/LogoWriter/LogoWriter.cs
@@ -1,17 +1,32 @@
-﻿using Colorful;
-using Contractor.Core.Helpers;
-using System.Drawing;
-using System.IO;
+﻿using System;
 
 namespace Contractor.CLI.Tools
 {
     internal static class LogoWriter
     {
+        private static readonly string Logo = string.Join(
+            Environment.NewLine,
+            @"_________               _____                        _____",
+            @"__  ____/______ _______ __  /_______________ __________  /_______ ________",
+            @"_  /     _  __ \__  __ \_  __/__  ___/_  __ `/_  ___/_  __/_  __ \__  ___/",
+            @"/ /___   / /_/ /_  / / // /_  _  /    / /_/ / / /__  / /_  / /_/ /_  /",
+            @"\____/   \____/ /_/ /_/ \__/  /_/     \__,_/  \___/  \__/  \____/ /_/");
+
         public static void Write()
         {
-            string FontPath = Path.Combine(Folder.Executable, "Tools", "LogoWriter", "speed.flf");
-            Console.WriteAscii("Contractor", FigletFont.Load(FontPath), Color.FromArgb(88, 116, 255));
-            System.Console.WriteLine("Version: " + GetAppVersion());
+            ConsoleWriteLineColor(Logo);
+            Console.WriteLine();
+            Console.WriteLine("Version: " + GetAppVersion());
+        }
+
+        private static void ConsoleWriteLineColor(string text)
+        {
+            ConsoleColor consoleColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine(text);
+
+            Console.ForegroundColor = consoleColor;
         }
 
         private static string GetAppVersion()
