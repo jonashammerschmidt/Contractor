d519389deb52925477b7494dff5a3a229ecfc151
Jonas Hammerschmidt <j.hammerschmidt@krz.de>
2024-03-25 15:50:33 +0100

    Added first some custom dto stuff

--- Contractor.Core/Generation/Options/MetaModell/CustomDtoPathItem.cs
+++ Contractor.Core/Generation/Options/MetaModell/CustomDtoPathItem.cs
@@ -5,5 +5,12 @@ namespace Contractor.Core.MetaModell
         public Entity Entity { get; set; }
 
         public Relation Relation { get; set; }
+
+        public string PropertyName { get; set; }
+
+        public override string ToString()
+        {
+            return Entity.Name + "." + PropertyName;
+        }
     }
 }
