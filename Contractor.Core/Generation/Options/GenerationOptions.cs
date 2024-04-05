146241cc1d9fe75015f3b604bc77e7768249d16a
Jonas Hammerschmidt <j.hammerschmidt@krz.de>
2024-04-05 10:57:54 +0200

    Added Unscoped->Scope Relation to FindRelation

--- Contractor.Core/Generation/Options/GenerationOptions.cs
+++ Contractor.Core/Generation/Options/GenerationOptions.cs
@@ -54,51 +54,129 @@ namespace Contractor.Core.MetaModell
         
         public Relation FindRelation(Entity searchedEntity, string searchedPropertyName)
         {
+            Relation relation = null;
             foreach (var module in Modules)
             {
                 foreach (var entity in module.Entities)
                 {
-                    var relations1To1From = entity.Relations1To1.SingleOrDefault(relation => 
-                        relation.EntityFrom.Name == searchedEntity.Name
-                        && (relation.PropertyNameTo ?? relation.EntityTo.Name).ToLower() == searchedPropertyName.ToLower());
-                    var relations1To1To = entity.Relations1To1.SingleOrDefault(relation => 
-                        relation.EntityTo.Name == searchedEntity.Name
-                        && (relation.PropertyNameFrom ?? relation.EntityFrom.Name).ToLower() == searchedPropertyName.ToLower());
-                    var relations1ToNFrom = entity.Relations1ToN.SingleOrDefault(relation => 
-                        relation.EntityFrom.Name == searchedEntity.Name
-                        && (relation.PropertyNameTo ?? relation.EntityTo.NamePlural).ToLower() == searchedPropertyName.ToLower());
-                    var relations1ToNTo = entity.Relations1ToN.SingleOrDefault(relation => 
-                        relation.EntityTo.Name == searchedEntity.Name
-                        && (relation.PropertyNameFrom ?? relation.EntityFrom.Name).ToLower() == searchedPropertyName.ToLower());
-                    var foundRelation = (relations1To1From as Relation)
-                        ?? (relations1To1To as Relation)
-                        ?? (relations1ToNFrom as Relation)
-                        ?? (relations1ToNTo as Relation);
-                    if (foundRelation != null)
-                    {
-                        return foundRelation;
-                    }
+                    relation = 
+                        FindSimpleRelation(searchedEntity, searchedPropertyName, entity) ??
+                        FindScopeRelation(searchedEntity, searchedPropertyName, entity);
                     
-                    if (entity.HasScope && entity == searchedEntity)
+                    if (relation != null)
                     {
-                        if (entity.ScopeEntity.Name.ToLower() == searchedPropertyName.ToLower())
-                        {
-                            return new Relation1ToN(entity.ScopeEntity, entity);
-                        }
+                        return relation;
                     }
+                }
+            }
+            
+            relation = FindScopeUnscopedRelation(searchedEntity, searchedPropertyName);
+            if (relation != null)
+            {
+                return relation;
+            }
 
-                    foreach (var scopedEntity in entity.ScopedEntities)
+            throw new KeyNotFoundException(searchedEntity.Name + "." + searchedPropertyName);
+        }
+
+        private Relation FindSimpleRelation(Entity searchedEntity, string searchedPropertyName, Entity entity)
+        {
+            var relations1To1From = entity.Relations1To1.SingleOrDefault(relation => 
+                relation.EntityFrom.Name == searchedEntity.Name
+                && (relation.PropertyNameTo ?? relation.EntityTo.Name).ToLower() == searchedPropertyName.ToLower());
+            var relations1To1To = entity.Relations1To1.SingleOrDefault(relation => 
+                relation.EntityTo.Name == searchedEntity.Name
+                && (relation.PropertyNameFrom ?? relation.EntityFrom.Name).ToLower() == searchedPropertyName.ToLower());
+            var relations1ToNFrom = entity.Relations1ToN.SingleOrDefault(relation => 
+                relation.EntityFrom.Name == searchedEntity.Name
+                && (relation.PropertyNameTo ?? relation.EntityTo.NamePlural).ToLower() == searchedPropertyName.ToLower());
+            var relations1ToNTo = entity.Relations1ToN.SingleOrDefault(relation => 
+                relation.EntityTo.Name == searchedEntity.Name
+                && (relation.PropertyNameFrom ?? relation.EntityFrom.Name).ToLower() == searchedPropertyName.ToLower());
+            var foundRelation = (relations1To1From as Relation)
+                                ?? (relations1To1To as Relation)
+                                ?? (relations1ToNFrom as Relation)
+                                ?? (relations1ToNTo as Relation);
+            return foundRelation;
+        }
+
+        private Relation FindScopeRelation(Entity searchedEntity, string searchedPropertyName, Entity otherEntity)
+        {
+            if (otherEntity.HasScope && otherEntity == searchedEntity)
+            {
+                if (otherEntity.ScopeEntity.Name.ToLower() == searchedPropertyName.ToLower())
+                {
                     {
-                        if (scopedEntity.NamePlural.ToLower() == searchedPropertyName.ToLower() && entity == searchedEntity)
-                        {
-                            return new Relation1ToN(entity, scopedEntity);
-                        }
+                        return new Relation1ToN(otherEntity.ScopeEntity, otherEntity);
                     }
                 }
             }
 
+            foreach (var scopedEntity in otherEntity.ScopedEntities)
+            {
+                if (scopedEntity.NamePlural.ToLower() == searchedPropertyName.ToLower() && otherEntity == searchedEntity)
+                {
+                    {
+                        return new Relation1ToN(otherEntity, scopedEntity);
+                    }
+                }
+            }
 
-            throw new KeyNotFoundException(searchedEntity.Name + "." + searchedPropertyName);
+            return null;
+        }
+
+        private Relation FindScopeUnscopedRelation(Entity searchedEntity, string searchedPropertyName)
+        {
+            foreach (var relation1To1 in searchedEntity.Relations1To1)
+            {
+                if ((relation1To1.EntityFrom.HasScope && !relation1To1.EntityTo.HasScope) || relation1To1.EntityTo.HasOtherScope(relation1To1.EntityFrom))
+                {
+                    Relation1To1 scopeRelation1To1 = new Relation1To1()
+                    {
+                        EntityFrom = relation1To1.EntityFrom.ScopeEntity,
+                        EntityNameFrom = relation1To1.EntityFrom.ScopeEntity.Name,
+                        PropertyNameFrom = (relation1To1.PropertyNameFrom ?? relation1To1.EntityFrom.Name) + relation1To1.EntityFrom.ScopeEntity.Name,
+                        EntityTo = relation1To1.EntityTo,
+                        PropertyNameTo = (relation1To1.PropertyNameTo ?? relation1To1.EntityTo.Name) + relation1To1.EntityFrom.Name,
+                        IsOptional = relation1To1.IsOptional,
+                        OnDelete = "NoAction",
+                        Order = relation1To1.Order,
+                        IsCreatedByPreProcessor = true,
+                        RelationBeforePreProcessor = relation1To1,
+                    };
+                    if (scopeRelation1To1.PropertyNameTo == searchedPropertyName)
+                    {
+                        return scopeRelation1To1;
+                    }
+                }
+            }
+
+            foreach (var relation1ToN in searchedEntity.Relations1ToN)
+            {
+                if ((relation1ToN.EntityFrom.HasScope && !relation1ToN.EntityTo.HasScope) || relation1ToN.EntityTo.HasOtherScope(relation1ToN.EntityFrom))
+                {
+                    Relation1ToN scopeRelation1ToN = new Relation1ToN()
+                    {
+                        EntityFrom = relation1ToN.EntityFrom.ScopeEntity,
+                        EntityNameFrom = relation1ToN.EntityFrom.ScopeEntity.Name,
+                        PropertyNameFrom = (relation1ToN.PropertyNameFrom ?? relation1ToN.EntityFrom.Name) + relation1ToN.EntityFrom.ScopeEntity.Name,
+                        EntityTo = relation1ToN.EntityTo,
+                        PropertyNameTo = (relation1ToN.PropertyNameTo ?? relation1ToN.EntityTo.Name) + relation1ToN.EntityFrom.Name,
+                        IsOptional = relation1ToN.IsOptional,
+                        OnDelete = "NoAction",
+                        Order = relation1ToN.Order,
+                        IsCreatedByPreProcessor = true,
+                        RelationBeforePreProcessor = relation1ToN,
+                    };
+
+                    if (scopeRelation1ToN.PropertyNameFrom == searchedPropertyName)
+                    {
+                        return scopeRelation1ToN;
+                    }
+                }
+            }
+
+            return null;
         }
 
         public void Sort(IEnumerable<Entity> sortedEntities)
